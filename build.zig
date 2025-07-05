4e18f1adb41f38a60c670431f0ee6747d3a6ef83
Russel Arbore <51011371+RArbore@users.noreply.github.com>
2025-07-05 20:56:22 +0000

    Initial commit

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "zsim",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    run_exe.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run zsim");
+    run_step.dependOn(&run_exe.step);
+}
